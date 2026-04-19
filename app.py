68028b58e660142cfef0956cadc5f8ceef4b4d8b
Monica Mohan <monicamons12@gmail.com>
2026-04-19 11:54:56 +0530

    fix: cors policy for carto basemap applied

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ CORS(app)
 
 @app.after_request
 def add_security_headers(response):
-    response.headers['Content-Security-Policy'] = "default-src 'self' 'unsafe-inline' 'unsafe-eval' https://unpkg.com https://basemaps.cartocdn.com"
+    response.headers['Content-Security-Policy'] = "default-src 'self' 'unsafe-inline' 'unsafe-eval' https: blob: data:;"
     response.headers['X-Content-Type-Options'] = 'nosniff'
     response.headers['X-Frame-Options'] = 'SAMEORIGIN'
     response.headers['Strict-Transport-Security'] = 'max-age=31536000; includeSubDomains'
